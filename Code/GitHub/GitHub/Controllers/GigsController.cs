a31276d81632a3183cad20838c420dee7d5f9464
jcfkbacksDev <jcfkbacks@gmail.com>
2017-07-25 12:04:37 -0500

    -Modificacion de GigFormViewModel, se agrego la propiedad Genres para mostrarlos en un dropdownlist en la vista -GigsController se obtienen los genres desde bd -Create view se agrego el control drowpdownlist para mostrar los genres desde la bd

--- Code/GitHub/GitHub/Controllers/GigsController.cs
+++ Code/GitHub/GitHub/Controllers/GigsController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GitHub.Models;
+using GitHub.ViewModels;
 
 namespace GitHub.Controllers
 {
     public class GigsController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
         // GET: Gigs
         public ActionResult Create()
         {
-            return View();
+            var vm = new GigFormViewModel()
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(vm);
         }
     }
 }
--- Code/GitHub/GitHub/ViewModels/GigFormViewModel.cs
+++ Code/GitHub/GitHub/ViewModels/GigFormViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using GitHub.Models;
 
 namespace GitHub.ViewModels
 {
@@ -10,6 +11,7 @@ namespace GitHub.ViewModels
         public string Venue { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
-
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
     }
 }
