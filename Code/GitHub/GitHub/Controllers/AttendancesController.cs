6bf192a136e27b338ed186eb5bef03b9da0df2c6
msiBacks <andres.rsa@outlook.com>
2017-07-26 00:23:24 -0500

    -LOOK Obtencion de Token desde un proyecto NO Web Api Se agregaron Nugets Clases para obtener el token bearer

--- Code/GitHub/GitHub/Controllers/AttendancesController.cs
+++ Code/GitHub/GitHub/Controllers/AttendancesController.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using System.Web.Http;
 using GitHub.Models;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace GitHub.Controllers
 {
     [Authorize]
     public class AttendancesController : ApiController
     {
-        private readonly ApplicationDbContext _context;
+        private ApplicationDbContext _context;
 
         public AttendancesController()
         {
@@ -20,12 +19,27 @@ namespace GitHub.Controllers
         }
 
         [HttpPost]
-        public IHttpActionResult Attend([FromBody]int gigId)
+        public async Task<IHttpActionResult> Attend([FromBody]int gigId)
         {
+            string att = null;
+            if (User.Identity.IsAuthenticated)
+            {
+                var claimsIdentity = User.Identity as ClaimsIdentity;
+                if (claimsIdentity != null)
+                {
+                    att = claimsIdentity.Claims.First().Value;
+                }
+            }
+            _context = new ApplicationDbContext();
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(_context));
+            var user = await userManager.FindByEmailAsync(att);
+            if (_context.Attendances.Any(a => a.AttendeeId == user.Id && a.GigId == gigId))
+                return BadRequest("The Attendance already exists.");
+
             var attendance = new Attendance()
             {
                 GigId = gigId,
-                AttendeeId = User.Identity.GetUserId()
+                AttendeeId = user.Id
             };
             _context.Attendances.Add(attendance);
             _context.SaveChanges();
