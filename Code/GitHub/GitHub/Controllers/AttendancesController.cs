c25e15befa62d2f7f67ceb0bdc6a89b714e79bc3
msiBacks <andres.rsa@outlook.com>
2017-07-25 23:46:17 -0500

    -LOOK creacion de controllador webapi Pero al no tener todas las librerias de un proyecto web Api No obtiene el Identity se agregan libs al proyecto http://bitoftech.net/2014/06/01/token-based-authentication-asp-net-web-api-2-owin-asp-net-identity/

---
+++ Code/GitHub/GitHub/Controllers/AttendancesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using GitHub.Models;
+using Microsoft.AspNet.Identity;
+
+namespace GitHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody]int gigId)
+        {
+            var attendance = new Attendance()
+            {
+                GigId = gigId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
