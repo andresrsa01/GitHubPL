9f37aba1c2dbf6de698d9d4b361966dea9190bee
jcfkbacksDev <jcfkbacks@gmail.com>
2017-07-25 20:23:50 -0500

    -LOOK shortcut para buscar miembros de una clase ctrl + f12 -Agrego campo extra ApplicationUser -Css corregido ya que no mostraba letras

--- Code/GitHub/GitHub/Controllers/HomeController.cs
+++ Code/GitHub/GitHub/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Data.Entity;
 using System.Web.Mvc;
+using GitHub.Models;
 
 namespace GitHub.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs
+                .Include(gig => gig.Artist)
+                .Where(gig => gig.DateTime > DateTime.Now);
+
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
